c94dc15fefbdb7f4782ae4c982e0cf814903509e
Santiago <santiago.g.19@hotmail.com>
2019-07-25 00:00:00 -0300

    Init

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ echo "" >> sshkey
 cat sshkey | tr -d '\r' | ssh-add - > /dev/null
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
+echo "" >> ./.env
+echo "APP_VERSION=$TRAVIS_TAG" >> ./.env
 echo "$PRIVATE_HOST_KEY" | base64 -di > ~/.ssh/known_hosts
 ssh openfing@openfing.fing.edu.uy 'rm -rf ~/openfing/preview; mkdir ~/openfing/preview'
 rsync -vaz -e ssh dist/ openfing@openfing.fing.edu.uy:~/openfing/preview
